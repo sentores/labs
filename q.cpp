3f1fc872ab3fad67476cae521110e5cbc5c53c04
Vladimir <wowka-k@bk.ru>
2013-05-19 23:00:56 +0400

    added comments
    qSort with comments

--- q.cpp
+++ q.cpp
@@ -14,20 +14,29 @@ void main()
 	high = n-1;
 	srand(time(0));
 	mass = (int*) malloc (n*sizeof(int));
-	for( int i = 0; i < n; i++ )
+	for( int i = 0; i < n; i++ )                       /// randomize the elements of massive
 	{
 		mass[i] = -50 + rand() % 100;
 		printf( "%d " , mass[i] );
 	}
-	qSort(mass,low,high);
-	PrintMass(mass,n);
+	qSort(mass,low,high);                              /// calling sorting function
+	PrintMass(mass,n);                                 /// calling printing function
 	getch();
 }
+
+/*! @fn void qSort(int* mass,int low,int high)
+    @brief sort massive
+    @param *mass - pointer to massive
+    @param low - index of low element
+	@param high - index of high element
+    @return void
+*/
+
 void qSort(int *mass, int low, int high)
 {
 	int i = low;
 	int j = high;
-	int m;
+	int m;                                             /// index of middle element
 	m = mass[(low+high)/2];
 	do 
 	{
@@ -46,10 +55,19 @@ void qSort(int *mass, int low, int high)
 	} 
 	while(i < j);
 	if( low < j ) 
-		qSort(mass,low,j);
+		qSort(mass,low,j);                             /// recursive calling
 	if(i < high) 
-		qSort(mass,i,high);
+		qSort(mass,i,high);                            /// recursive calling
 }
+
+
+/*! @fn void PrintMass(int *mass, int n)
+    @brief print massive 
+    @param *mass - pointer to massive
+    @param n - number of elements in massive
+    @return void
+*/
+
 void PrintMass(int *mass, int n)
 {
 	printf("\n");
