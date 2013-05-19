cb21b3e0289b1d3caed1e3b0adcd7bacfb5b611c
Vladimir <wowka-k@bk.ru>
2013-05-19 23:02:01 +0400

    added comments
    mergesort with comments

--- mergesort.cpp
+++ mergesort.cpp
@@ -7,8 +7,8 @@ void MergeSort(int*,int,int);
 void PrintMass(int*, int);
 void main()
 {
-	int n;
-	int *mass;
+	int n;                                            /// number of elemnts
+	int *mass;                                        /// pointer to massive
 	printf("n=");
 	scanf("%d", &n);
 	srand(time(0));
@@ -18,10 +18,21 @@ void main()
 		mass[i] = -50 + rand() % 100;
 		printf( "%d " , mass[i] );
 	}
-	MergeSort(mass,0,n-1);
-	PrintMass(mass,n);
+	MergeSort(mass,0,n-1);                            /// calling sorting function
+	PrintMass(mass,n);                                /// calling printing function
 	getch();
 }
+
+
+/*! @fn void MergeMass(int* SourceMass,int left,int middle,int right)
+    @brief merge parts
+    @param *SourceMass - pointer to massive
+    @param left - left index
+	@param middle - index of middle
+	@param right - right index
+    @return void
+*/
+
 void MergeMass (int* SourceMass,int left,int middle,int right)
 {
 	int* masstM = new int[right-left];
@@ -57,15 +68,33 @@ void MergeMass (int* SourceMass,int left,int middle,int right)
 	for(int c = 0; c <= right-left; c++)
 		SourceMass[c+left] = masstM[c];
 }
+
+/*! @fn void MergeSort(int* SourceMass,int left,int right)
+    @brief sorting in interval [left,right]
+    @param *SourceMass - pointer to massive
+    @param left - left index
+	@param right - right index
+    @return void
+*/
+
 void MergeSort(int* SourceMass,int left,int right)
 {
 	if(left >= right)
 		return;
 	int mid = (right+left)/2;
-	MergeSort(SourceMass,left,mid);
-	MergeSort(SourceMass,mid+1,right);
+	MergeSort(SourceMass,left,mid);                        /// recursive calling
+	MergeSort(SourceMass,mid+1,right);                     /// recursive calling
 	MergeMass(SourceMass,left,mid,right);
 }
+
+
+/*! @fn void PrintMass(int *mass, int n)
+    @brief print massive 
+    @param *mass - pointer to massive
+    @param n - number of elements in massive
+    @return void
+*/
+
 void PrintMass(int *mass, int n)
 {
 	printf("\n");
