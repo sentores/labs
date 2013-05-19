48b74b509999b7d2c74b85cccc2c772ac1bbdb50
Vladimir <wowka-k@bk.ru>
2013-05-19 23:01:31 +0400

    added comments
    bubble sort with comments

--- bubble.cpp
+++ bubble.cpp
@@ -6,21 +6,30 @@ void SortMass(int*, int);
 void PrintMass(int*, int);
 void main()
 {
-	int n;
-	int *mass;
+	int n;                                  /// number of elements
+	int *mass;                              /// pointer to massive
 	printf("n=");
 	scanf("%d", &n);
 	srand(time(0));
 	mass = (int*) malloc (n*sizeof(int));
-	for( int i = 0; i < n; i++ )
+	for( int i = 0; i < n; i++ )            /// randomize the elements of massive
 	{
 		mass[i] = -50 + rand() % 100;
 		printf( "%d " , mass[i] );
 	}
-	SortMass(mass,n);
-	PrintMass(mass,n);
+	SortMass(mass,n);                       /// sort massive
+	PrintMass(mass,n);                      /// print result
 	getch();
 }
+
+
+/*! @fn void SortMass(int *mass, int n)
+    @brief sort massive 
+    @param *mass - pointer to massive
+    @param n - number of elements in massive
+    @return void
+*/
+
 void SortMass(int *mass, int n)
 {
 	int tmp,t = 0;
@@ -39,6 +48,15 @@ void SortMass(int *mass, int n)
 	}
 	while ( t < n  );
 }
+
+
+/*! @fn void PrintMass(int *mass, int n)
+    @brief print massive 
+    @param *mass - pointer to massive
+    @param n - number of elements in massive
+    @return void
+*/
+
 void PrintMass(int *mass, int n)
 {
 	printf("\n");
